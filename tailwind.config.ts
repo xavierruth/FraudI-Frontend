0d4b9b1231b5e09ede348c2c34e93d8c1bca4a5d
xavierruth <ruthmarianne.xavier@gmail.com>
2025-04-04 00:30:13 -0300

    Ajustes no tailwind e postcss

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
